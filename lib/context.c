67b3e0b5da7ef43a5af0da3ceda64ee39e711abb
Nathanael Jones <nathanael.jones@gmail.com>
2016-03-24 15:38:52 -0600

    Fix warning, drop old allocations comments

--- lib/context.c
+++ lib/context.c
@@ -85,9 +85,7 @@ bool flow_context_has_error(flow_context* context) { return context->error.reaso
 
 static const char* status_code_to_string(flow_status_code code)
 {
-    if (code < 0) {
-        return "Unknown negative status code";
-    }
+    //Code is an unsigned enum, and cannot be negative. We just check the upper bounds
     if (code >= flow_status_First_user_defined_error && code <= flow_status_Last_user_defined_error) {
         return "User defined error";
     }
@@ -547,28 +545,3 @@ void flow_context_profiler_stop(flow_context* context, const char* name, bool as
 }
 
 flow_profiling_log* flow_context_get_profiler_log(flow_context* context) { return &context->log; }
-
-/* Aligned allocations
-
-#include <stdlib.h>
-#include "malloc.h"
-
-#define ir_malloc(size) _aligned_malloc(size, 32)
-#define ir_free(ptr) _aligned_free(ptr)
-
-
-_declspec(noalias) _declspec(restrict) inline void* _ir_aligned_calloc(size_t count, size_t elsize, size_t alignment){
-    if (elsize == 0 || count >= SIZE_MAX / elsize) { return NULL; } // Watch out for overflow
-    size_t size = count * elsize;
-    void *memory = _aligned_malloc(size, alignment);
-    if (memory != NULL) { memset(memory, 0, size); }
-    return memory;
-}
-
-#define ir_calloc(count, element_size) _ir_aligned_calloc(count,element_size, 32)
-#else
-#define ir_malloc(size) malloc(size)
-#define ir_free(ptr) free(ptr)
-#define ir_calloc(count, element_size) calloc(count,element_size)
-#endif
-*/
