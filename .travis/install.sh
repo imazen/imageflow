76886bf570be334c9344ec2a02afcbce1f8f8616
Nathanael Jones <nathanael.jones@gmail.com>
2016-03-30 13:28:36 -0600

    Travis: add coveralls

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,9 +6,17 @@ set -x
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
     brew install cmake || true
+    brew install lcov || true
     brew install conan
 else
+    cd ${TRAVIS_BUILD_DIR}
+    # install latest LCOV
+    wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
+    tar xf lcov_1.11.orig.tar.gz
+    sudo make -C lcov-1.11/ install
     sudo docker pull lasote/conangcc${GCC_VERSION}
 fi
 
+#install lcov to coveralls conversion + upload tool
+gem install coveralls-lcov
 
