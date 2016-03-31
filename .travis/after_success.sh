56f37e17aa5b3d81bcb282cba8627aa3d18a09ae
Lasote <lasote@gmail.com>
2016-03-31 15:08:25 +0200

    Give permissions

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -2,7 +2,7 @@
 if [[ "$(uname -s)" != 'Darwin' ]]; then
     if ! [ -z "$TRAVIS_BUILD_DIR" ] && [ "${TRAVIS_BUILD_DIR+xxx}" = "xxx" ]; then
       cd ${TRAVIS_BUILD_DIR}
-      # sudo chmod -R a+rw .
+      sudo chmod -R a+rw .
       lcov --directory ./build --capture --output-file coverage.info # capture coverage info
       lcov --remove coverage.info 'tests/*' '.conan/*' '/usr/*' --output-file coverage.info # filter out system and test code
       lcov --list coverage.info # debug before upload
