87289bb50c74420717861dbb050b64b12e0b3249
Nathanael Jones <nathanael.jones@gmail.com>
2016-03-31 14:44:10 -0600

    Improve rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,16 @@
+rm -rf build
+rm libimageflow.so
+rm conaninfo.txt
+rm conanbuildinfo.cmake
+rm *.user
+rm conanfile.pyc
+rm -rf node_frames
+rm tests/visuals/*.png
+rm tests/visuals/*.html
+rm tests/visuals/*~
+rm cacert.pem
+rm -rf bin
+rm *~
 mkdir build
 cd build
 conan install -u --file ../conanfile.py -o build_tests=True --build missing
