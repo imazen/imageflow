8653eb0e28f32eeabe0037495a1f9bb9bc067966
Nathanael Jones <nathanael.jones@gmail.com>
2016-04-06 16:49:00 -0600

    Travis: install DSSIM first

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -1,4 +1,13 @@
 mkdir -p build
 cd build
+
+#Download and build DSSIM
+wget https://github.com/pornel/dssim/archive/master.tar.gz
+tar xvzf master.tar.gz
+cd dssim-master
+make
+cd ..
+cp dssim-master/bin/dssim ./dssim
+
 conan install --file ../conanfile.py -o build_tests=True --build missing -u
 conan build --file ../conanfile.py
