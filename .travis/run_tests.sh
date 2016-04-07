b8c816c56b04c92e883c2623199f43fba8d5a29d
Nathanael Jones <nathanael.jones@gmail.com>
2016-04-06 18:15:18 -0600

    Travis: make sure dssim is in PATH

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -6,8 +6,9 @@ wget https://github.com/pornel/dssim/archive/master.tar.gz
 tar xvzf master.tar.gz
 cd dssim-master
 make
-cd ..
-cp dssim-master/bin/dssim ./dssim
+cd bin
+export PATH=$PATH:$(pwd)
+cd ../..
 
 conan install --file ../conanfile.py -o build_tests=True --build missing -u
 conan build --file ../conanfile.py
