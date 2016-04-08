03caf90b383a80a5c3e65ca999dfa7016712cff3
Nathanael Jones <nathanael.jones@gmail.com>
2016-04-07 18:52:03 -0600

    imageflow-vips: benchmark with multiple images

--- wrappers/ruby/spec/vips/benchmark_spec.rb
+++ wrappers/ruby/spec/vips/benchmark_spec.rb
@@ -10,55 +10,57 @@ module Imageflow::Vips
       dir = File.expand_path("temp_vips_benchmarking")
       Dir.mkdir(dir) unless Dir.exist? dir
 
-      input_file_basename = "u1.jpg"
+      ["gamma_dalai_lama_gray.jpg", "u1.jpg", "mountain_800.jpg"].each do |input_file_basename|
 
-      input_file = "#{dir}/#{input_file_basename}"
-      url = "http://s3.amazonaws.com/resizer-images/#{input_file_basename}"
-      puts "Fetching test image #{url}\n"
-      IO.binwrite(input_file, Net::HTTP.get(URI(url))) unless File.exist? input_file
+        input_file = "#{dir}/#{input_file_basename}"
+        url = "https://s3-us-west-2.amazonaws.com/imageflow-resources/test_inputs/#{input_file_basename}"
+        puts "Fetching test image #{url}\n"
+        IO.binwrite(input_file, Net::HTTP.get(URI(url))) unless File.exist? input_file
 
 
-      puts `vipsthumbnail --vips-version`
-      skip if $?.exitstatus != 0
+        puts `vipsthumbnail --vips-version`
+        skip if $?.exitstatus != 0
 
-      executables = ["vipsthumbnail", "imageflow-vips"]
+        executables = ["vipsthumbnail", "imageflow-vips"]
 
-      output_path_for = lambda { |exe, index| exe =~ /imageflow-vips/ ? "#{dir}/imageflow_%s_#{index}.png" : "#{dir}/vips_%s_#{index}.png" }
+        output_path_for = lambda { |exe, index| exe =~ /imageflow-vips/ ? "#{dir}/imageflow_%s_#{index}.png" : "#{dir}/vips_%s_#{index}.png" }
 
-      argument_sets = ["", "--linear"]
+        argument_sets = ["", "--linear"]
 
-      argument_sets.each_with_index do |argset, index|
-        puts "\nComparing both with arguments: #{argset}\n"
-        Benchmark.bmbm(80) do |bench|
-          executables.each do |program|
+        argument_sets.each_with_index do |argset, index|
+          puts "\nComparing both with arguments: #{argset}\n"
+          Benchmark.bmbm(80) do |bench|
+            executables.each do |program|
 
-            args = "#{argset} --output=#{output_path_for.call(program, index)} #{input_file}"
-            puts "Using #{program} #{args}\n"
+              args = "#{argset} --output=#{output_path_for.call(program, index)} #{input_file}"
+              puts "Using #{program} #{args}\n"
 
-            bench.report(File.basename(program)) do
-              result = 0
-              out_msg = "";
+              bench.report(File.basename(program)) do
+                result = 0
+                out_msg = "";
 
-              #exclude loading rubygems
-              if program =~ /imageflow-vips/
-                result = Imageflow::Vips::Cli.start(args.split(/\s+/))
-              else
-                out_msg = `#{program} #{args}`
-                result = $?.exitstatus
-              end
-              if result != 0
-                raise "#{out_msg}\nFailed with exit code #{result}: #{program} #{args}"
+                #exclude loading rubygems
+                if program =~ /imageflow-vips/
+                  result = Imageflow::Vips::Cli.start(args.split(/\s+/))
+                else
+                  out_msg = `#{program} #{args}`
+                  result = $?.exitstatus
+                end
+                if result != 0
+                  raise "#{out_msg}\nFailed with exit code #{result}: #{program} #{args}"
+                end
               end
             end
           end
+
+          sleep(1)
+
+          paths = executables.map { |exe| output_path_for.call(exe, index).gsub(/%s/, input_file_basename.gsub(/\.[a-zA-Z]+$/, '')) }
+          puts "\nComparing result images...\n"
+          puts `dssim #{paths[0]} #{paths[1]}`
+          puts "            #{paths[0]}\n"
+
         end
-
-        sleep(1)
-
-        paths = executables.map { |exe| output_path_for.call(exe, index).gsub(/%s/, input_file_basename.gsub(/\.[a-zA-Z]+$/, '')) }
-        puts "\nComparing result images...\n"
-        puts `dssim #{paths[0]} #{paths[1]}`
-        puts "            #{paths[0]}\n"
 
       end
     end
