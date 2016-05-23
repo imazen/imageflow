3f5dd391f3c7876b1fa449f0a960a401072ed93b
unknown <tostercx@gmail.com>
2016-05-23 22:21:50 +0300

    Fix MSVC macro

--- lib/compositing.c
+++ lib/compositing.c
@@ -14,8 +14,8 @@
 #include <string.h>
 
 #ifdef _MSC_VER
-#define likely(x) x
-#define unlikely(x) x
+#define likely(x) (x)
+#define unlikely(x) (x)
 #else
 #define likely(x) (__builtin_expect(!!(x), 1))
 #define unlikely(x) (__builtin_expect(!!(x), 0))
