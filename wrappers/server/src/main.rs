285993da8ccbd1a4e1b853327b6b5e9d928b7f11
Nathanael Jones <nathanael.jones@gmail.com>
2016-06-03 22:47:03 -0600

    cargo new

---
+++ wrappers/server/src/main.rs
@@ -0,0 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
+fn main() {
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello world!")))
+    }).http("localhost:3000").unwrap();
+}
