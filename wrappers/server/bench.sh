80d38f8c471d60a89dfebe9543fb364b99c7743e
Nathanael Jones <nathanael.jones@gmail.com>
2016-06-24 16:44:06 -0600

    Add bench_single_thread

--- wrappers/server/bench.sh
+++ wrappers/server/bench.sh
@@ -13,6 +13,8 @@ echo
 echo on OS X, you will need to edit this script to use time instead of perf stat
 echo And run brew install parallel
 
+#-limit thread 1
+
 cargo build --release
 cp target/release/flow-proto1 .
 
@@ -36,13 +38,13 @@ export COUNT=32
 for i in `seq 1 $COUNT`;
 do
      cp u1.jpg bench_in/c$i.jpg
-done 
+done
 
 
 
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  export TIME_COMMAND="perf stat" 
+  export TIME_COMMAND="perf stat"
 else
   export TIME_COMMAND=time
 fi
@@ -52,17 +54,17 @@ cd bench_in
 echo Using imageflow to thumbnail $COUNT images in parallel
 $TIME_COMMAND parallel '../flow-proto1 -i {} -o ../bench_out/{.}_200x200.jpg -w 200 -h 200' ::: *.jpg
 echo
-echo 
+echo
 echo Using libvips to thumbnail $COUNT images in parallel
 $TIME_COMMAND parallel 'vipsthumbnail --linear --size=200x200  --output=../bench_out/{.}_vips_200x200.jpg {}' ::: *.jpg
 
 echo
-echo 
+echo
 echo Using ImageMagick to thumbnail $COUNT images in parallel
 $TIME_COMMAND parallel 'convert {} -set colorspace sRGB -colorspace RGB -filter Robidoux -resize 200x200  -colorspace sRGB ../bench_out/{.}_magick_200x200.jpg' ::: *.jpg
 
 echo
-echo 
+echo
 echo Using ImageMagick ideal settings to thumbnail $COUNT images in parallel
 $TIME_COMMAND parallel 'convert {} -set colorspace sRGB -colorspace RGB -filter  Mitchell -distort Resize 200x200  -colorspace sRGB ../bench_out/{.}_magick_ideal_200x200.jpg' ::: *.jpg
 
@@ -72,7 +74,7 @@ echo
 echo Using imageflow to create 2000px versions of $COUNT images in parallel
 $TIME_COMMAND parallel '../flow-proto1 -i {} -o ../bench_out/{.}_2000x2000.jpg -w 2000 -h 2000' ::: *.jpg
 echo
-echo 
+echo
 echo Using libvips to create 2000px versions of $COUNT images in parallel
 $TIME_COMMAND parallel 'vipsthumbnail --linear --size=2000x2000  --output=../bench_out/{.}_vips_2000x2000.jpg {}' ::: *.jpg
 
@@ -82,7 +84,7 @@ echo Using ImageMagick to create 2000px versions of $COUNT images in parallel
 $TIME_COMMAND parallel 'convert {} -set colorspace sRGB -colorspace RGB -filter Robidoux -resize 2000x2000  -colorspace sRGB ../bench_out/{.}_magick_2000x2000.jpg' ::: *.jpg
 
 echo
-echo 
+echo
 echo Using ImageMagick ideal settings to create 2000px versions of COUNT images in parallel
 $TIME_COMMAND parallel 'convert {} -set colorspace sRGB -colorspace RGB -filter  Mitchell -distort Resize 2000x2000  -colorspace sRGB ../bench_out/{.}_magick_ideal_2000x2000.jpg' ::: *.jpg
 
@@ -92,7 +94,7 @@ echo
 echo Using imageflow wrong on $COUNT images in parallel. 200x200
 $TIME_COMMAND parallel '../flow-proto1 -i {} --incorrectgamma -o ../bench_out/{.}_200x200_wrong.jpg -w 200 -h 200' ::: *.jpg
 echo
-echo 
+echo
 echo Using libvips wrong on $COUNT images in parallel. 200x200
 $TIME_COMMAND parallel 'vipsthumbnail --size=200x200  --output=../bench_out/{.}_vips_200x200_wrong.jpg {}' ::: *.jpg
 
