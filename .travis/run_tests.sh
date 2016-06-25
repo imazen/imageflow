98d6b8ca339ffa769a0b18f2c1073f63c0131cb9
Nathanael Jones <nathanael.jones@gmail.com>
2016-06-25 11:51:08 -0600

    Roll back to older DSSIM. Master branch of DSSIM can't be compiled on 14.04

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -2,10 +2,10 @@ mkdir -p build
 cd build
 
 #Download and build DSSIM
-sudo apt-get install -y pkg-config libpng16-dev
-wget https://github.com/pornel/dssim/archive/master.tar.gz
-tar xvzf master.tar.gz
-cd dssim-master
+sudo apt-get install -y pkg-config libpng-dev
+wget https://github.com/pornel/dssim/archive/c6ad29c5a2dc37d8610120486f09eda145621c84.tar.gz
+tar xvzf c6ad29c5a2dc37d8610120486f09eda145621c84.tar.gz
+cd dssim-c6ad29c5a2dc37d8610120486f09eda145621c84
 make
 cd bin
 export PATH=$PATH:$(pwd)
