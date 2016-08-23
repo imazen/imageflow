fb206d2825639c8e601c05c0a74e1dc87fa56602
Nathanael Jones <nathanael.jones@gmail.com>
2016-08-22 22:45:54 -0600

    Don't even try to build openssl. Use new segmented crates

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -8,10 +8,7 @@ cd ${TRAVIS_BUILD_DIR}
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
     brew install cmake || true
-    brew install conan nasm openssl
-    brew link --force openssl
-    export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
-    export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
+    brew install conan nasm
     ./ci/install_dssim.sh
 else
     docker pull ${DOCKER_IMAGE}
