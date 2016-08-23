9d9068afc46495201839709085c15c14a1c3633b
Nathanael Jones <nathanael.jones@gmail.com>
2016-08-22 20:00:55 -0600

    Try different approach with AppVeyor

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -10,6 +10,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew install cmake || true
     brew install conan nasm openssl
     brew link --force openssl
+    export OPENSSL_INCLUDE_DIR=`brew --prefix openssl`/include
+    export OPENSSL_LIB_DIR=`brew --prefix openssl`/lib
     ./ci/install_dssim.sh
 else
     docker pull ${DOCKER_IMAGE}
