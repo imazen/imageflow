c9650ac3111c3d205d2b5a1406f18edce6857034
Nathanael Jones <nathanael.jones@gmail.com>
2016-08-23 14:12:18 -0600

    Make tar happier

--- ci/travis_before_deploy.sh
+++ ci/travis_before_deploy.sh
@@ -7,5 +7,7 @@ set -x
 if [ -z "$PACKAGE_SUFFIX" ]; then
   rm -rf ${TRAVIS_BUILD_DIR}/artifacts
 else
-  tar czf ${TRAVIS_BUILD_DIR}/artifacts/${PROJECT_NAME}-build${TRAVIS_BUILD_ID}-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}-${PACKAGE_SUFFIX}.tar.gz ${TRAVIS_BUILD_DIR}/artifacts/staging/*
+  cd ${TRAVIS_BUILD_DIR}/artifacts/staging
+  tar czf ${TRAVIS_BUILD_DIR}/artifacts/${PROJECT_NAME}-build${TRAVIS_BUILD_ID}-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}-${PACKAGE_SUFFIX}.tar.gz *
+  cd ${TRAVIS_BUILD_DIR}
 fi
