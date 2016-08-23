abed52b01cb10a55db046574c7d0f3ce7d0f0e62
Nathanael Jones <nathanael.jones@gmail.com>
2016-08-23 14:45:05 -0600

    Improve Travis/Appveyor artifact packaging

--- ci/travis_before_deploy.sh
+++ ci/travis_before_deploy.sh
@@ -8,6 +8,6 @@ if [ -z "$PACKAGE_SUFFIX" ]; then
   rm -rf ${TRAVIS_BUILD_DIR}/artifacts
 else
   cd ${TRAVIS_BUILD_DIR}/artifacts/staging
-  tar czf ${TRAVIS_BUILD_DIR}/artifacts/${PROJECT_NAME}-build${TRAVIS_BUILD_ID}-${TRAVIS_BRANCH}-${TRAVIS_COMMIT}-${PACKAGE_SUFFIX}.tar.gz *
+  tar czf ${TRAVIS_BUILD_DIR}/artifacts/${PACKAGE_PREFIX}-${TRAVIS_BRANCH}-travisjob-${TRAVIS_JOB_NUMBER}-${TRAVIS_COMMIT}-${PACKAGE_SUFFIX}.tar.gz *
   cd ${TRAVIS_BUILD_DIR}
 fi
