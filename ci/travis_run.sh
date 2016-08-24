111987edb746e509e8cbb3d6d53f99357bcd707f
Nathanael Jones <nathanael.jones@gmail.com>
2016-08-23 23:12:58 -0600

    travis

--- ci/travis_run.sh
+++ ci/travis_run.sh
@@ -11,4 +11,4 @@ else
     docker run --rm -v $HOME/.ccache:/home/conan/.ccache -v $HOME/.conan/data:/home/conan/.conan/data -v ${TRAVIS_BUILD_DIR}:/home/conan/imageflow -e "COVERALLS_TOKEN=${COVERALLS_TOKEN}" -e "JOB_NAME=${JOB_NAME}"  -e "UPLOAD_BUILD=${UPLOAD_BUILD}" -e "RUST_CHANNEL=${RUST_CHANNEL}" -e "COVERALLS=${COVERALLS}" -e "COVERALLS=${COVERALLS}" -e "VALGRIND=${VALGRIND}" ${DOCKER_IMAGE} /bin/bash -c "./ci/travis_run_docker.sh"
 fi
 
-bash ./travis_tar_artifacts.sh
+bash ./ci/travis_tar_artifacts.sh
