354dc8448cf8c4867a5bbe049c6c1543a68d91c5
Nathanael Jones <nathanael.jones@gmail.com>
2016-09-02 22:34:21 -0600

    Fix script typo

--- ci/docker/build_if_gcc48/test.sh
+++ ci/docker/build_if_gcc48/test.sh
@@ -4,4 +4,4 @@ set -x
 export VALGRIND=false
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export IMAGE_NAME="$(basename ${SCRIPT_DIR})"
-. ${SCRIPT_DIR}/../test.sh ${IMAGE_NAME} FOO=${VARIABLE:-nightly} 
+. ${SCRIPT_DIR}/../test.sh ${IMAGE_NAME} ${VARIABLE:-nightly} 
