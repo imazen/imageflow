4b08e1a119fe5e3ef1ec41d0c6c6c532710e9da1
Nathanael Jones <nathanael.jones@gmail.com>
2016-09-03 18:41:22 -0600

    Travis

--- ci/travis_run.sh
+++ ci/travis_run.sh
@@ -22,7 +22,7 @@ if [ -n "${TRAVIS_TAG}" ]; then
 else
 	export GIT_BRANCH_NAME=${TRAVIS_BRANCH}
 	export UPLOAD_AS_LATEST=True
-else
+fi
 
 #Don't upload pull requests
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
@@ -32,9 +32,12 @@ else
 	export UPLOAD_AS_LATEST=false
 fi
 
-#
+if [ -n "${TRAVIS_BUILD_DIR}"]; then
+  cd ${TRAVIS_BUILD_DIR}
+fi
+
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    ${TRAVIS_BUILD_DIR}/ci/travis_run_osx.sh
+    ./ci/travis_run_osx.sh
 else
     docker run --rm -v $HOME/.ccache:/home/conan/.ccache -v $HOME/.conan/data:/home/conan/.conan/data -v ${TRAVIS_BUILD_DIR}:/home/conan/imageflow -e "COVERALLS_TOKEN=${COVERALLS_TOKEN}" -e "JOB_NAME=${JOB_NAME}"  -e "UPLOAD_BUILD=${UPLOAD_BUILD}" -e "RUST_CHANNEL=${RUST_CHANNEL}" -e "COVERALLS=${COVERALLS}" -e "VALGRIND=${VALGRIND}" -e "GIT_BRANCH_NAME=${GIT_BRANCH_NAME}" -e "GIT_COMMIT=${GIT_COMMIT}" -e "JOB_BADGE=${JOB_BADGE}" -e "PACKAGE_PREFIX=${PACKAGE_PREFIX}"  -e "PACKAGE_SUFFIX=${PACKAGE_SUFFIX}" -e "UPLOAD_AS_LATEST=${UPLOAD_AS_LATEST}"  ${DOCKER_IMAGE} /bin/bash -c "./ci/travis_run_docker.sh"
 fi
