4bebc68dff2b412525e3b59e00708e359a813a4f
Nathanael Jones <nathanael.jones@gmail.com>
2016-09-03 22:01:37 -0600

    Add more windows build scripts

--- win_build_c.bat
+++ win_build_c.bat
@@ -8,5 +8,3 @@ set CONAN_ARCH=x86_64
 conan install --scope build_tests=True -o shared=True --build missing -s build_type=Release -s arch=%CONAN_ARCH%  -u ../
 conan build ../
 cd ..
-
-cmd /K
---
+++ win_build_rust.bat
@@ -0,0 +1,35 @@
+cmd.exe /c "win_verify_tools.bat"
+
+REM set CONAN_ARCH=x86
+set CONAN_ARCH=x86_64
+SET RUST_TARGET=x86_64-pc-windows-msvc
+
+conan export imazen/testing
+cd imageflow_core
+conan install --build missing -s build_type=Release -s arch=%CONAN_ARCH%
+cargo test
+cargo build --target=%RUST_TARGET% --release
+cargo doc --no-deps
+cd ..
+cd imageflow_tool
+cargo test
+cargo build --target=%RUST_TARGET% --release
+cargo doc --no-deps
+cd ..
+cd imageflow_cdylib
+cargo test
+cargo build --target=%RUST_TARGET% --release
+cargo doc --no-deps
+cd ..
+
+echo Copying to artifacts\staging
+
+mkdir artifacts
+mkdir artifacts\staging
+mkdir artifacts\staging\doc
+dir target\%RUST_TARGET%\release\
+xcopy target\%RUST_TARGET%\release\flow-proto1.exe  artifacts\staging\
+xcopy target\%RUST_TARGET%\release\imageflowrs.dll  artifacts\staging\
+xcopy target\%RUST_TARGET%\release\imageflow_tool.exe  artifacts\staging\
+xcopy /d target\doc  artifacts\staging\doc
+
