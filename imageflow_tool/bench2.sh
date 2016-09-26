6db9e6250bd36a1dc57a690149aa0deba246434f
Nathanael Jones <nathanael.jones@gmail.com>
2016-09-25 20:32:50 -0600

    Fix bench2.sh

--- imageflow_tool/bench2.sh
+++ imageflow_tool/bench2.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 cargo build --release
-cp target/release/flow-proto1 .
+cp ../target/release/flow-proto1 .
 
 ./flow-proto1 --version
-#wget -nc  https://s3-us-west-2.amazonaws.com/imageflow-resources/test_inputs/u1.jpg
 
-./flow-proto1 -i galaxy.jpg -o g_bench2.jpg -w 400 -h 400 --benchmark-threads=8 --benchmark-count=32
+wget -nc  https://s3-us-west-2.amazonaws.com/imageflow-resources/test_inputs/u1.jpg
 
-./flow-proto1 -i galaxy.jpg -o g2_bench2.jpg -w 400 -h 400 --benchmark-threads=1 --benchmark-count=12
+./flow-proto1 -i u1.jpg -o u1__bench2.jpg -w 400 -h 400 --benchmark-threads=8 --benchmark-count=32
 
-./flow-proto1 -i galaxy.jpg -o g3_bench2.jpg -w 400 -h 400 --benchmark-threads=1 --benchmark-count=12 --incorrectgamma
+./flow-proto1 -i u1.jpg -o u1_2_bench2.jpg -w 400 -h 400 --benchmark-threads=1 --benchmark-count=12
+
+./flow-proto1 -i u1.jpg -o u1_3_bench2.jpg -w 400 -h 400 --benchmark-threads=1 --benchmark-count=12 --incorrectgamma
