917af6d9db1dc6d2449a8e38f5d3379923df0814
Nathanael Jones <nathanael.jones@gmail.com>
2016-11-12 14:22:41 -0700

    Direct download/configure/make is fine until we go static - different warnings, though

--- tests/libpng/testread.sh
+++ tests/libpng/testread.sh
@@ -4,18 +4,21 @@ set -e #Exit on failure.
 gcc -v
 
 
-#wget -nc https://sourceforge.net/projects/libpng/files/libpng16/older-releases/1.6.23/libpng-1.6.23.tar.gz
+wget -nc https://sourceforge.net/projects/libpng/files/libpng16/older-releases/1.6.23/libpng-1.6.23.tar.gz
 
-#tar -xvzf libpng-1.6.23.tar.gz
-#cd libpng-1.6.23.tar.gz
-
-conan install
-
+tar -xvzf libpng-1.6.23.tar.gz
+cd libpng-1.6.23
+./configure --prefix=$(pwd)/..
+make
+make install
+cd ..
 echo Compiling
 
-gcc testread.c -g @conanbuildinfo.gcc  -o testread.out
+gcc testread.c -lpng16 -lz -lm -static -L./lib -I./include -o testread.out
 
 #-fsanitize=address
+
+export LD_LIBRARY_PATH=./lib:${LD_LIBRARY_PATH}
 
 ./testread.out
 
