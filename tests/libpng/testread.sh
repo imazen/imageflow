0c903d0d0ffe4601cdaf0346b0754e13a5be8b93
Nathanael Jones <nathanael.jones@gmail.com>
2016-11-12 14:41:41 -0700

    Building zlib from source reproduces this

--- tests/libpng/testread.sh
+++ tests/libpng/testread.sh
@@ -5,21 +5,21 @@ gcc -v
 
 
 wget -nc https://sourceforge.net/projects/libpng/files/libpng16/older-releases/1.6.23/libpng-1.6.23.tar.gz
-
+wget -nc http://zlib.net/zlib128.zip
 tar -xvzf libpng-1.6.23.tar.gz
 cd libpng-1.6.23
 CFLAGS="-fPIC" ./configure --prefix=$(pwd)/..
 make
 make install
 cd ..
-echo Compiling
+unzip zlib128.zip
+cd zlib-1.2.8
+CFLAGS="-fPIC" ./configure --prefix=$(pwd)/..
+make
+make install
+cd ..
 
 gcc testread.c -lpng16 -lz -lm -static -L./lib -I./include -o testread.out
-
-
-./testread.out
-
-echo Running via Valgrind
 valgrind ./testread.out --track-origins=yes
 
 
