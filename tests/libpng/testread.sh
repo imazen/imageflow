f2875d871a60dbcf185c86edba336fe7d6e30844
Nathanael Jones <nathanael.jones@gmail.com>
2016-11-12 14:45:53 -0700

    No solution yet for zlib; restore to suppressions.txt

--- tests/libpng/testread.sh
+++ tests/libpng/testread.sh
@@ -19,7 +19,13 @@ make
 make install
 cd ..
 
+#static require to reproduce the problem
+
 gcc testread.c -lpng16 -lz -lm -static -L./lib -I./include -o testread.out
+
+#Helps if we're using the shared libs
+export LD_LIBRARY_PATH=./lib:${LD_LIBRARY_PATH}
+
 valgrind ./testread.out --track-origins=yes
 
 
