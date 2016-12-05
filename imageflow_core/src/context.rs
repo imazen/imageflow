83550c3cc6e75ed242349f44f5ee146c2e4ebb38
Nathanael Jones <nathanael.jones@gmail.com>
2016-12-04 23:24:22 -0700

    refactor(core): delete dead code (old Context struct)

--- imageflow_core/src/context.rs
+++ imageflow_core/src/context.rs
@@ -10,9 +10,6 @@ pub struct ContextPtr {
     // (i.e, unit test helpers are ported, or the helper becomes cfgtest on the struct itself)
     pub ptr: Option<*mut ::ffi::ImageflowContext>,
 }
-struct Context {
-    p: cell::RefCell<ContextPtr>,
-}
 
 pub struct SelfDisposingContextPtr {
     ptr: ContextPtr,
@@ -49,6 +46,19 @@ impl Drop for SelfDisposingContextPtr {
 pub struct JobPtr {
     ptr: *mut ::ffi::ImageflowJob,
     c: *mut ::ffi::ImageflowContext,
+}
+
+
+pub struct Job {
+    pub p: cell::RefCell<JobPtr>,
+}
+pub struct JobIoPtr {
+    pub ptr: Option<*mut ::ffi::ImageflowJobIo>,
+}
+
+pub struct JobIo<'a, T: 'a> {
+    pub p: cell::RefCell<JobIoPtr>,
+    pub _marker: marker::PhantomData<&'a T>,
 }
 
 
@@ -322,39 +332,12 @@ impl ContextPtr {
             imageflow_response as *const ImageflowJsonResponse
         }
     }
-}
 
-pub struct Job {
-    pub p: cell::RefCell<JobPtr>,
-}
-pub struct JobIoPtr {
-    pub ptr: Option<*mut ::ffi::ImageflowJobIo>,
-}
-
-pub struct JobIo<'a, T: 'a> {
-    pub p: cell::RefCell<JobIoPtr>,
-    pub _marker: marker::PhantomData<&'a T>,
-}
-
-
-
-impl Context {
-    pub fn message(&mut self, method: &str, json: &[u8]) -> Result<JsonResponse> {
-        let b = &mut (*self.p.borrow_mut());
-        b.message(method, json)
-    }
-}
-
-
-
-impl ContextPtr {
     pub fn message(&mut self, method: &str, json: &[u8]) -> Result<JsonResponse> {
 
         ::context_methods::CONTEXT_ROUTER.invoke(self, method, json)
     }
-}
 
-impl ContextPtr {
     pub fn create() -> Result<ContextPtr> {
         unsafe {
             let ptr = ::ffi::flow_context_create();
@@ -406,185 +389,7 @@ impl ContextPtr {
             _ => Err(FlowError::ContextInvalid),
         }
     }
-}
 
-
-
-impl Drop for Context {
-    fn drop(&mut self) {
-        (*self.p.borrow_mut()).force_destroy();
-    }
-}
-impl Context {
-    pub fn create() -> Result<Context> {
-        unsafe {
-            let ptr = ::ffi::flow_context_create();
-
-            if ptr.is_null() {
-                Err(FlowError::Oom)
-            } else {
-                Ok(Context { p: cell::RefCell::new(ContextPtr { ptr: Some(ptr) }) })
-            }
-        }
-    }
-
-    pub fn unsafe_borrow_mut_context_pointer(&mut self) -> std::cell::RefMut<ContextPtr> {
-        self.p.borrow_mut()
-    }
-
-    fn get_error_copy(&self) -> Option<FlowError> {
-        (*self.p.borrow()).get_error_copy()
-    }
-
-    pub fn destroy(self) -> Result<()> {
-        let b = &mut (*self.p.borrow_mut());
-        match b.ptr {
-            None => Ok(()),
-            Some(ptr) => unsafe {
-                if !::ffi::flow_context_begin_terminate(ptr) {
-                    // Already borrowed; will panic!
-                    // This kind of bug is only exposed at runtime, now.
-                    // Code reuse will require two copies of every function
-                    // One against the ContextPtr, to be reused
-                    // One exposed publicly against the Context, which performs the borrowing
-                    // Same scenario will occur with other types.
-                    // let copy = self.get_error_copy().unwrap();
-
-                    // So use the ContextPtr version
-                    let copy = b.get_error_copy().unwrap();
-                    b.force_destroy();
-                    Err(copy)
-                } else {
-                    b.force_destroy();
-                    Ok(())
-                }
-            },
-        }
-    }
-
-    pub fn create_job(&mut self) -> Result<Job> {
-        let b = &(*self.p.borrow_mut());
-        match b.ptr {
-            None => Err(FlowError::ContextInvalid),
-            Some(ptr) => unsafe {
-                let p = ::ffi::flow_job_create(ptr);
-                if p.is_null() {
-                    Err(b.get_error_copy().unwrap())
-                } else {
-                    Ok(Job { p: cell::RefCell::new(JobPtr::from_ptr(ptr, p).unwrap()) })
-                }
-            },
-        }
-    }
-
-
-    pub fn create_io_from_slice<'a, 'c>(&'c mut self,
-                                        bytes: &'a [u8])
-                                        -> Result<JobIo<'a, &'a [u8]>> {
-        let b = &(*self.p.borrow_mut());
-        match b.ptr {
-            None => Err(FlowError::ContextInvalid),
-            Some(ptr) => unsafe {
-                let p = ::ffi::flow_io_create_from_memory(ptr,
-                                                          ::ffi::IoMode::ReadSeekable,
-                                                          bytes.as_ptr(),
-                                                          bytes.len(),
-                                                          ptr as *const libc::c_void,
-                                                          ptr::null());
-                if p.is_null() {
-                    Err(b.get_error_copy().unwrap())
-                } else {
-                    Ok(JobIo {
-                        _marker: marker::PhantomData,
-                        p: cell::RefCell::new(JobIoPtr { ptr: Some(p) }),
-                    })
-                }
-            },
-        }
-    }
-
-
-    pub fn create_io_output_buffer<'a, 'b>(&'a mut self) -> Result<JobIo<'b, ()>> {
-        let b = &(*self.p.borrow_mut());
-        match b.ptr {
-            None => Err(FlowError::ContextInvalid),
-            Some(ptr) => unsafe {
-                let p = ::ffi::flow_io_create_for_output_buffer(ptr, ptr as *const libc::c_void);
-                if p.is_null() {
-                    Err(b.get_error_copy().unwrap())
-                } else {
-                    Ok(JobIo {
-                        _marker: marker::PhantomData,
-                        p: cell::RefCell::new(JobIoPtr { ptr: Some(p) }),
-                    })
-                }
-            },
-        }
-    }
-
-    pub fn job_add_io<T>(&mut self,
-                         job: &mut Job,
-                         io: JobIo<T>,
-                         io_id: i32,
-                         direction: IoDirection)
-                         -> Result<()> {
-        let b = &(*self.p.borrow_mut());
-        match b.ptr {
-            None => Err(FlowError::ContextInvalid),
-            Some(ptr) => unsafe {
-                let p = ::ffi::flow_job_add_io(ptr,
-                                               (*job.p.borrow_mut()).ptr,
-                                               (*io.p.borrow_mut()).ptr.unwrap(),
-                                               io_id,
-                                               direction);
-                if !p {
-                    Err(b.get_error_copy().unwrap())
-                } else {
-                    Ok(())
-                }
-            },
-        }
-    }
-
-
-    pub fn io_get_output_buffer<'a, 'b>(&'a mut self,
-                                        job: &'b Job,
-                                        io_id: i32)
-                                        -> Result<&'b [u8]> {
-        let b = &(*self.p.borrow_mut());
-        match b.ptr {
-            None => Err(FlowError::ContextInvalid),
-            Some(ptr) => unsafe {
-
-                let io_p = ::ffi::flow_job_get_io(ptr, (*job.p.borrow_mut()).ptr, io_id);
-                if io_p.is_null() {
-                    Err(b.get_error_copy().unwrap())
-                } else {
-                    let mut buf_start: *const u8 = ptr::null();
-                    let mut buf_len: usize = 0;
-                    let worked = ::ffi::flow_io_get_output_buffer(ptr,
-                                                                  io_p,
-                                                                  &mut buf_start as *mut *const u8,
-                                                                  &mut buf_len as *mut usize);
-                    if !worked {
-                        Err(b.get_error_copy().unwrap())
-                    } else if buf_start.is_null() {
-                        // Not sure how output buffer is null... no writes yet?
-                        Err(FlowError::ErrNotImpl)
-                    } else {
-                        Ok((std::slice::from_raw_parts(buf_start, buf_len)))
-                    }
-                }
-
-
-
-            },
-        }
-    }
-}
-
-
-impl ContextPtr {
     unsafe fn get_flow_err(&self, c: *mut ::ffi::ImageflowContext) -> FlowErr {
 
 
@@ -666,30 +471,3 @@ impl ContextPtr {
 
 
 
-#[test]
-fn it_works() {
-    let mut c = Context::create().unwrap();
-
-    let mut j = c.create_job().unwrap();
-
-
-    let bytes = [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49,
-                 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06,
-                 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00, 0x00, 0x00, 0x0A, 0x49, 0x44,
-                 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01, 0x0D,
-                 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42,
-                 0x60, 0x82];
-
-    let input = c.create_io_from_slice(&bytes).unwrap();
-
-    let output = c.create_io_output_buffer().unwrap();
-
-    c.job_add_io(&mut j, input, 0, IoDirection::In).unwrap();
-    c.job_add_io(&mut j, output, 1, IoDirection::Out).unwrap();
-
-
-    // let output_bytes = c.io_get_output_buffer(&j, 1).unwrap();
-
-    assert_eq!(c.destroy(), Ok(()));
-
-}
