607a46bd5fdcb88765e74269ffef4beefe12892c
Nathanael Jones <nathanael.jones@gmail.com>
2017-01-05 18:57:31 -0700

    docker: add imageflow_server_unsecured Dockerfile & trigger for Docker Hub

---
+++ ci/travis_trigger_docker_cloud.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e #Exit on failure.
+
+if [[ -z "$TRAVIS_PULL_REQUEST_SHA" ]]; then
+	if [[ -n "$TRAVIS_TAG" ]]; then
+		export CLOUD_SOURCE_NAME="${TRAVIS_TAG}"
+		export CLOUD_SOURCE_TYPE="Tag"
+	else
+		if [[ -n "$TRAVIS_BRANCH" ]]; then
+			export CLOUD_SOURCE_NAME="${TRAVIS_BRANCH}"
+			export CLOUD_SOURCE_TYPE="Branch"
+		fi
+	fi
+fi 
+
+if [[ -n "$CLOUD_SOURCE_NAME" ]]; then
+	PAYLOAD="{'source_type': '${CLOUD_SOURCE_TYPE}', 'source_name': '${CLOUD_SOURCE_NAME}'}"
+
+	# This token has no security value and is rate limited to 10. 
+	# It only checks GitHub for the given tag/branch - it does not accept any data.
+	TRIGGER_ENDPOINT=https://registry.hub.docker.com/u/nathanaeljones/imageflow_server_unsecured/trigger/4cef52f0-76f6-4f2e-9a5e-e2b93b7d2f59/
+
+	curl -H "Content-Type: application/json" --data "${PAYLOAD}" -X POST "$TRIGGER_ENDPOINT"
+fi 
+
