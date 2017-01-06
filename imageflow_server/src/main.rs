950fb1b45cdd24030f5f63f1e0c5e816b49b6637
Nathanael Jones <nathanael.jones@gmail.com>
2017-01-05 22:31:36 -0700

    imageflow_server: only force localhost for tagged builds

--- imageflow_server/src/main.rs
+++ imageflow_server/src/main.rs
@@ -103,10 +103,16 @@ fn main_with_exit_code() -> i32 {
         let data_dir = m.value_of("data-dir").map(|s| PathBuf::from(s));
         let bind = m.value_of("bind-address").map(|s| s.to_owned()).expect("bind address required");
 
-        if bind != "localhost" && bind != "127.0.0.1" && bind != "::1"{
-            println!("This build of imageflow_server only permits connections from localhost (address {} rejected). It is not secure.", &bind);
-            std::process::exit(64);
+        let is_release = option_env!("GIT_OPTIONAL_TAG").is_some() && !option_env!("GIT_OPTIONAL_TAG").unwrap().is_empty();
+        let vars_present = option_env!("GIT_COMMIT").is_some();
+
+        if is_release || !vars_present {
+            if bind != "localhost" && bind != "127.0.0.1" && bind != "::1" {
+                println!("This build of imageflow_server only permits connections from localhost (address {} rejected). It is not secure.", &bind);
+                std::process::exit(64);
+            }
         }
+
         let combined = format!("{}:{}", bind, port);
 
         {
