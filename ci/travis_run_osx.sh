987364ea31f9491baeb091c60f54977f8ed4ed0f
Nathanael Jones <nathanael.jones@gmail.com>
2017-01-07 17:06:08 -0700

    travis: osx also needs new conan settings

--- ci/travis_run_osx.sh
+++ ci/travis_run_osx.sh
@@ -6,5 +6,10 @@ export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
 export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
 export OPENSSL_LIB_DIR=/usr/local/opt/openssl/lib
 
+#Copy conan settings - always
+cp "./ci/updated_conan_settings.yml" "${HOME}/.conan/settings.yml"
+
+
+
 ./build.sh
 
