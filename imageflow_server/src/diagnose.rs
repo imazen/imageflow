975ab5bbb429ad86323af33ffcceb91209093dd8
Nathanael Jones <nathanael.jones@gmail.com>
2017-01-08 17:52:37 -0700

    server: smoke-test-core should now exercise jpeg paths

--- imageflow_server/src/diagnose.rs
+++ imageflow_server/src/diagnose.rs
@@ -8,6 +8,25 @@ use ::rustc_serialize::base64::FromBase64;
 
 const BLUE_PNG32_200X200_B64:&'static str = "iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAABiUlEQVR42u3TgRAAQAgAsA/qkaKLK48EIug2h8XP6gesQhAQBAQBQUAQEAQEAUFAEBAEEAQEAUFAEBAEBAFBQBAQBAQRBAQBQUAQEAQEAUFAEBAEBAEEAUFAEBAEBAFBQBAQBAQBQQBBQBAQBAQBQUAQEAQEAUEAQUAQEAQEAUFAEBAEBAFBQBBAEBAEBAFBQBAQBAQBQUAQQBAQBAQBQUAQEAQEAUFAEBAEEAQEAUFAEBAEBAFBQBAQBAQRBAQBQUAQEAQEAUFAEBAEBAEEAUFAEBAEBAFBQBAQBAQBQQQBQUAQEAQEAUFAEBAEBAFBAEFAEBAEBAFBQBAQBAQBQUAQQBAQBAQBQUAQEAQEAUFAEEAQEAQEAUFAEBAEBAFBQBAQBBAEBAFBQBAQBAQBQUAQEAQQBAQBQUAQEAQEAUFAEBAEBAEEAUFAEBAEBAFBQBAQBAQBQQQBQUAQEAQEAUFAEBAEBAFBAEFAEBAEBAFBQBAQBAQBQUAQQUAQEAQEAUFAEBAEBIGLBkZ+sahOjkyUAAAAAElFTkSuQmCC";
 
+
+fn smoke_jpeg_to_jpeg() {
+    let framewise = fluent::fluently()
+        .decode(0)
+        .constrain_within(Some(40), Some(40), Some(s::ConstraintResamplingHints::with(None, Some(25f32))))
+        .encode(1, s::EncoderPreset::libjpegturbo()).builder().to_framewise();
+
+    let bytes = include_bytes!("assets/tiny.jpg");
+
+    let req = stateless::BuildRequest{
+        export_graphs_to: None,
+        inputs: vec![stateless::BuildInput{bytes: bytes, io_id: 0}],
+        framewise: framewise
+    };
+    let _ = stateless::LibClient{}.build(req).unwrap();
+}
+
+
+
 fn smoke_png_to_png() {
     let framewise = fluent::fluently()
         .decode(0)
@@ -26,4 +45,5 @@ fn smoke_png_to_png() {
 
 pub fn smoke_test_core() {
     smoke_png_to_png();
+    smoke_jpeg_to_jpeg();
 }
