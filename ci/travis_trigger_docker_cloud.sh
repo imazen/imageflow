da651adcef4492a2ac4eb16009beaaaad900dde5
Nathanael Jones <nathanael.jones@gmail.com>
2017-02-13 12:48:46 -0700

    Add docker/hub/tool

--- ci/travis_trigger_docker_cloud.sh
+++ ci/travis_trigger_docker_cloud.sh
@@ -25,7 +25,14 @@ if [[ -n "$CLOUD_SOURCE_NAME" ]]; then
 	# This token has no security value and is rate limited to 10. 
 	# It only checks GitHub for the given tag/branch - it does not accept any data.
 	TRIGGER_ENDPOINT=https://registry.hub.docker.com/u/imazen/imageflow_server_unsecured/trigger/3682f725-3a98-49dd-9e96-acd594721250/
-	echo "Triggering docker cloud build with $PAYLOAD"
+	echo "Trigger 1 (server): docker cloud build with $PAYLOAD"
 	curl -H "Content-Type: application/json" --data "${PAYLOAD}" -X POST "$TRIGGER_ENDPOINT"
+
+	TRIGGER_ENDPOINT_2=https://registry.hub.docker.com/u/imazen/imageflow_tool/trigger/d4943bd2-6350-4cda-9012-f56fe2deaef8/
+	
+	echo "Trigger 2 (imageflow_tool) docker cloud build with $PAYLOAD"
+	curl -H "Content-Type: application/json" --data "${PAYLOAD}" -X POST "$TRIGGER_ENDPOINT_2"
+
+
 fi 
 
