b6443ff47860957b5ff6e979def8cda5eb3cbe86
Nathanael Jones <nathanael.jones@gmail.com>
2017-03-10 13:45:03 -0700

    travis: trim OS X install tasks

--- ci/travis_run_osx.sh
+++ ci/travis_run_osx.sh
@@ -2,14 +2,8 @@
 set -e #Exit on failure.
 echo "travis_run_osx.sh:"
 
-export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
-export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
-export OPENSSL_LIB_DIR=/usr/local/opt/openssl/lib
-
 #Copy conan settings - always
 cp "./ci/updated_conan_settings.yml" "${HOME}/.conan/settings.yml"
-
-
 
 ./build.sh
 
