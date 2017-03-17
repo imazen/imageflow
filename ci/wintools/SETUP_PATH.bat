8b35772ff45a660a5e44ce48bd0807d465d7bf9e
Nathanael Jones <nathanael.jones@gmail.com>
2017-03-17 15:28:17 -0600

    build: update windows scrpts
    Update windows build scripts

--- ci/wintools/SETUP_PATH.bat
+++ ci/wintools/SETUP_PATH.bat
@@ -10,6 +10,23 @@ set PATH=%PATH%;C:\Program Files (x86)\Conan\conan
 echo Updated path to
 echo %PATH%
 echo .
+
+set CONAN_ARCH=x86
+set RUST_TARGET=i686-pc-windows-msvc
+set TARGET_CPU=sandybridge
+set RUST_FLAGS=%RUST_FLAGS -C target-cpu=%TARGET_CPU%
+
+set CARGO_INCREMENTAL=1
+set RUST_TEST_THREADS=1
+set VS_ARCH=x86
+
+if [%1] == [x86] goto :x86 
+set CONAN_ARCH=x86_64
+set RUST_TARGET=x86_64-pc-windows-msvc
+set VS_ARCH=amd64
+:x86
+
+echo VS_ARCH=%VSARCH% CONAN_ARCH=%CONAN_ARCH% RUST_TARGET=%RUST_TARGET% TARGET_CPU=%TARGET_CPU% CARGO_INCREMENTAL=%CARGO_INCREMENTAL%
 echo NOW entering VS 14
 
-%comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"" amd64
+%comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %VS_ARCH%"
--- win_build_c.bat
+++ win_build_c.bat
@@ -1,15 +1,20 @@
 cmd.exe /c "ci\wintools\win_verify_tools.bat"
 
-cd c_components
-mkdir build
-cd build
+REM EXPECTS CONAN_ARCH and TARGET_CPU from ci\wintools\SETUP_PATH.bat
 
-REM set CONAN_ARCH=x86
-set CONAN_ARCH=x86_64
-conan install --scope build_tests=True -o shared=True --build missing -s build_type=Release -s arch=%CONAN_ARCH%  -u ../
-conan build ../
-cd ..
-cd ..
+cd c_components || exit /b
+mkdir build
+cd build || exit /b
+
+conan install --generator txt --scope build_tests=True -o shared=True --build missing -s build_type=Release -s arch=%CONAN_ARCH%  -s target_cpu=%TARGET_CPU% -u ../ || exit /b
+
+conan build ../ || exit /b
+cd .. || exit /b
 
 echo Clearing cached C component
 conan remove imageflow_c/* -f
+conan export imazen/testing  || exit /b
+cd ..  || exit /b 
+cd imageflow_core  || exit /b
+conan install --build missing -s build_type=Release -s arch=%CONAN_ARCH%  -s target_cpu=%TARGET_CPU% || exit /b
+cd .. || exit /b
