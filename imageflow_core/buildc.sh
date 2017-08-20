1bf650a4bccdb9187b4ea1809c58cb5671a5cec6
Nathanael Jones <nathanael.jones@gmail.com>
2017-08-20 12:50:05 -0600

    Fix buildc.sh, test_tool config

--- imageflow_core/buildc.sh
+++ imageflow_core/buildc.sh
@@ -11,6 +11,6 @@ conan export imazen/testing
 
 cd ../imageflow_core
 
-conan install --build missing # Will build imageflow package with your current settings
+conan install --build missing -s target_cpu=haswell # Will build imageflow package with your current settings
 
 CARGO_INCREMENTAL=1 cargo test
