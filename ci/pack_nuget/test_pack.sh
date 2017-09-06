c512d2803037e1ead6d11eb7879c9f2bed5f67f3
Nathanael Jones <nathanael.jones@gmail.com>
2017-09-06 13:38:52 -0600

    NuGet package builder: drop <files> element completely

--- ci/pack_nuget/test_pack.sh
+++ ci/pack_nuget/test_pack.sh
@@ -2,7 +2,7 @@
 set -e #Exit on failure.
 
 export CI_TAG="v0.1-prerelease0"
-export PACKAGE_SUFFIX="x86_64-linux-gcc48-eglibc219"
-export NUGET_RUNTIME="win9-x64"
-echo "hello" > ./../../target/release/imageflow.dll
+export PACKAGE_SUFFIX="ubuntu_14_04-x86_64"
+export NUGET_RUNTIME="ubuntu.14.04-x64"
+
 ./pack.sh
