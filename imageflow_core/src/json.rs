87968516c215e3c50919f8f071019b3a6cd48071
Lilith River <lilith@imazen.io>
2019-07-22 23:16:38 -0600

    Update MethodRouter to be Send and therefore compatible with Hashbrown

--- imageflow_core/src/json.rs
+++ imageflow_core/src/json.rs
@@ -3,8 +3,8 @@
 
 use internal_prelude::works_everywhere::*;
 
-type ResponderFn<'a, T, D> = Box<Fn(&mut T, D) -> Result<s::ResponsePayload> + 'a + Sync>;
-type MethodHandler<'a, T> = Box<Fn(&mut T, &[u8]) -> (JsonResponse, std::result::Result<(), FlowError>) + 'a + Sync>;
+type ResponderFn<'a, T, D> = Box<Fn(&mut T, D) -> Result<s::ResponsePayload> + 'a + Sync + Send>;
+type MethodHandler<'a, T> = Box<Fn(&mut T, &[u8]) -> (JsonResponse, std::result::Result<(), FlowError>) + 'a + Sync + Send>;
 
 #[cfg_attr(feature = "cargo-clippy", allow(new_without_default_derive))] // clippy is broke
 #[derive(Default)]
