c34a173f5b748a82eb6e83159f10f0af9f401562
Lilith River <lilith@imazen.io>
2019-11-18 18:38:55 -0700

    Another attempt at publishing to docker

--- ci/travis_publish_docker_images.sh
+++ ci/travis_publish_docker_images.sh
@@ -9,6 +9,11 @@ if [[ -z "$2" ]]; then
     echo "travis_publish_docker_images.sh requires a docker image name as a second parameter. Exiting." && exit 1;
 fi
 
+if [[ "$PUBLISH_DOCKER" == "True" ]]; then
+    echo Publishing to docker hub enabled
+else
+    exit 1;
+fi
 
 # Test locally by running
 #  TRAVIS_BRANCH=master ./ci/travis_trigger_docker_cloud.sh https://registry.hub.docker.com/u/imazen/imageflow_server_unsecured/trigger/3682f725-3a98-49dd-9e96-acd594721250/
@@ -25,14 +30,16 @@ if [[ -z "$TRAVIS_PULL_REQUEST_SHA" ]]; then
 fi
 if [[ -n "$PUBLISH_DOCKER_TAG" ]]; then
 
+
     if [[ -z "$SOURCE_COMMIT" ]]; then
         export SOURCE_COMMIT="${SOURCE_COMMIT:-$(git rev-parse HEAD)}"
         echo "Updating SOURCE_COMMIT from git rev-parse HEAD"
         echo "SOURCE_COMMIT: $SOURCE_COMMIT"
     fi
 
-echo "Building image $2:$PUBLISH_DOCKER_TAG in directory $1"
+    echo "Building image $2:$PUBLISH_DOCKER_TAG in directory $1"
 
-(cd $1 && docker build -t "$2:$PUBLISH_DOCKER_TAG" --build-arg "SOURCE_COMMIT=$SOURCE_COMMIT" --build-arg "DOCKER_TAG=$PUBLISH_DOCKER_TAG" . && docker push "$2:$PUBLISH_DOCKER_TAG")
+    (cd $1 && docker build -t "$2:$PUBLISH_DOCKER_TAG" --build-arg "SOURCE_COMMIT=$SOURCE_COMMIT" --build-arg "DOCKER_TAG=$PUBLISH_DOCKER_TAG" . && docker push "$2:$PUBLISH_DOCKER_TAG")
 
 
+fi
