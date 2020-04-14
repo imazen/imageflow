181f6382f8f97a56ac3cbb3da2cec3e5a731be60
Lilith River <lilith@imazen.io>
2020-04-14 06:27:53 -0600

    Delete ci/travis_trigger_docker_cloud.sh; dead code

--- ci/travis_publish_docker_images.sh
+++ ci/travis_publish_docker_images.sh
@@ -16,10 +16,6 @@ else
     exit 1;
 fi
 
-# Test locally by running
-#  TRAVIS_BRANCH=master ./ci/travis_trigger_docker_cloud.sh https://registry.hub.docker.com/u/imazen/imageflow_server_unsecured/trigger/3682f725-3a98-49dd-9e96-acd594721250/
-#  TRAVIS_TAG=v0.0.10 ./ci/travis_trigger_docker_cloud.sh https://registry.hub.docker.com/u/imazen/imageflow_tool/trigger/d4943bd2-6350-4cda-9012-f56fe2deaef8/
-
 if [[ -z "$TRAVIS_PULL_REQUEST_SHA" ]]; then
 	if [[ -n "$TRAVIS_TAG" ]]; then
 		export PUBLISH_DOCKER_TAG="${TRAVIS_TAG}"
