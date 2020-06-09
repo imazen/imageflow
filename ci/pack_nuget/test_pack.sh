e2a6d8a162b71fd86ebdeba154bb2b0eeaf31b4c
Lilith River <lilith@imazen.io>
2020-06-09 11:49:24 -0600

    Update test_pack.sh

--- ci/pack_nuget/test_pack.sh
+++ ci/pack_nuget/test_pack.sh
@@ -3,7 +3,7 @@ set -e #Exit on failure.
 
 #Underscores are prohibited in prerelease tags
 export CI_TAG="v0.9-rc1-1"
-export PACKAGE_SUFFIX="ubuntu_14_04-x86_64"
-export NUGET_RUNTIME="ubuntu.14.04-x64"
+export PACKAGE_SUFFIX="ubuntu_16_04-x86_64"
+export NUGET_RUNTIME="ubuntu.16.04-x64"
 
-./pack.sh tool
+./pack.sh
