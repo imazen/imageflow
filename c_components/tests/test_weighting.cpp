25c31b13a19d5f33c7558b41086c0482d8997139
Lilith River <lilith@imazen.io>
2020-09-24 17:20:29 -0600

    Improve reference weights file syntax for clarity

--- c_components/tests/test_weighting.cpp
+++ c_components/tests/test_weighting.cpp
@@ -210,12 +210,12 @@ TEST_CASE("Test output weights", "[fastscaling]")
                 ERR(c);
             }
 
-            fprintf(output, "\nfilter_%02d,%3d,%3d, ", filter_id, from_width, to_width);
+            fprintf(output, "\nfilter_%02d (%2dpx to %2dpx): ", filter_id, from_width, to_width);
 
             for (uint32_t output_pixel = 0; output_pixel < lct->LineLength; output_pixel++) {
                 struct flow_interpolation_pixel_contributions * current = &lct->ContribRow[output_pixel];
 
-                fprintf(output, "to %i: ", output_pixel);
+                fprintf(output, "x=%i from ", output_pixel);
 
                 for (int32_t ix = current->Left; ix <= current->Right; ix++) {
                     float weight = current->Weights[ix - current->Left];
