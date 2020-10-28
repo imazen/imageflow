44c57d9f51df61803f23e6865ec15d979142d4df
Lilith River <lilith@imazen.io>
2020-10-28 13:00:42 -0600

    Cargo.lock

--- imageflow_core/benches/bench_graphics.rs
+++ imageflow_core/benches/bench_graphics.rs
@@ -5,11 +5,12 @@ use imageflow_types::*;
 use imageflow_core::Context;
 use criterion::{ criterion_group, criterion_main, Criterion};
 use imageflow_core::graphics::bitmaps::*;
+use imageflow_core::graphics::scaling::ScaleAndRenderParams;
 
 
 fn benchmark_transpose(ctx: &mut Criterion) {
-    for w in (1u32..3000u32).step_by(1373) {
-        for h in (1u32..3000u32).step_by(1373) {
+    for w in (1000u32..3000u32).step_by(1373) {
+        for h in (1000u32..3000u32).step_by(1373) {
             let c = Context::create().unwrap();
             let mut a = Bitmap::create_u8(w, h, PixelLayout::BGRA, true, true, ColorSpace::LinearRGB,BitmapCompositing::ReplaceSelf).unwrap();
             let mut a = unsafe {
@@ -22,7 +23,11 @@ fn benchmark_transpose(ctx: &mut Criterion) {
                 let a_bgra=b.get_window_u8().unwrap().to_bitmap_bgra().unwrap();
                 a_bgra
             };
-            ctx.bench_function(&format!("transpose w={} && h={}", w, h), |bn| bn.iter(|| { unsafe { assert_eq!(flow_bitmap_bgra_transpose(c.flow_c(), &mut a as *mut BitmapBgra, &mut b as *mut BitmapBgra), true) } }));
+            ctx.bench_function(&format!("transpose w={} && h={}", w, h), |bn| bn.iter(|| {
+                unsafe {
+                    assert_eq!(flow_bitmap_bgra_transpose(c.flow_c(), &mut a as *mut BitmapBgra, &mut b as *mut BitmapBgra), true)
+                }
+            }));
         }
     }
 }
@@ -42,7 +47,9 @@ fn benchmark_flip_v(ctx: &mut Criterion) {
                     a_bgra.fill_rect( 0u32, 0u32, w, h, &Color::Srgb(ColorSrgb::Hex("FF0000FF".to_string()))).unwrap();
                     a_bgra
                 };
-                ctx.bench_function(&format!("flip_v w={} && h={} fmt={:?}",w,h,fmt), |bn| bn.iter(|| { unsafe { assert_eq!(flow_bitmap_bgra_flip_vertical(c.flow_c(), &mut a as *mut BitmapBgra),true) } } ));
+                ctx.bench_function(&format!("flip_v w={} && h={} fmt={:?}",w,h,fmt), |bn| bn.iter(|| {
+                    unsafe { assert_eq!(flow_bitmap_bgra_flip_vertical(c.flow_c(), &mut a as *mut BitmapBgra),true) }
+                } ));
             }
         }
     }
@@ -63,7 +70,9 @@ fn benchmark_flip_h(ctx: &mut Criterion) {
                     a_bgra.fill_rect( 0u32, 0u32, w, h, &Color::Srgb(ColorSrgb::Hex("FF0000FF".to_string()))).unwrap();
                     a_bgra
                 };
-                ctx.bench_function(&format!("flip_h w={} && h={} fmt={:?}",w,h,fmt), |bn| bn.iter(|| { unsafe { assert_eq!(flow_bitmap_bgra_flip_horizontal(c.flow_c(), &mut a as *mut BitmapBgra),true) } } ));
+                ctx.bench_function(&format!("flip_h w={} && h={} fmt={:?}",w,h,fmt), |bn| bn.iter(|| {
+                    unsafe { assert_eq!(flow_bitmap_bgra_flip_horizontal(c.flow_c(), &mut a as *mut BitmapBgra),true) }
+                } ));
             }
         }
     }
@@ -75,21 +84,21 @@ fn benchmark_scale_2d(ctx: &mut Criterion) {
     let float_spaces=[Floatspace::Srgb,Floatspace::Linear];
     for &float_space in float_spaces.iter(){
         for &fmt in fmts.iter(){
-            for w in (2000u32..4000u32).step_by(1373){
-                for h in (2000u32..4000u32).step_by(1373){
+            for w in (500u32..4000u32).step_by(2400){
+                for h in (500u32..4000u32).step_by(2400){
                     let c = Context::create().unwrap();
-                    let mut a = Bitmap::create_u8(w, h, PixelLayout::BGRA, true, true, ColorSpace::LinearRGB,BitmapCompositing::ReplaceSelf).unwrap();
+                    let mut bitmap_a = Bitmap::create_u8(w, h, PixelLayout::BGRA, true, true, ColorSpace::LinearRGB,BitmapCompositing::ReplaceSelf).unwrap();
                     let mut a = unsafe {
-                        let mut a_bgra = a.get_window_u8().unwrap().to_bitmap_bgra().unwrap();
+                        let mut a_bgra = bitmap_a.get_window_u8().unwrap().to_bitmap_bgra().unwrap();
                         a_bgra.fill_rect( 0u32, 0u32, w, h, &Color::Srgb(ColorSrgb::Hex("FF0000FF".to_string()))).unwrap();
                         a_bgra
                     };
-                    let mut b = Bitmap::create_u8(800u32,800u32, PixelLayout::BGRA, true, true, ColorSpace::LinearRGB,BitmapCompositing::ReplaceSelf).unwrap();
+                    let mut bitmap_b = Bitmap::create_u8(800u32,800u32, PixelLayout::BGRA, true, true, ColorSpace::LinearRGB,BitmapCompositing::ReplaceSelf).unwrap();
                     let mut b = unsafe {
-                        let a_bgra=b.get_window_u8().unwrap().to_bitmap_bgra().unwrap();
+                        let a_bgra=bitmap_b.get_window_u8().unwrap().to_bitmap_bgra().unwrap();
                         a_bgra
                     };
-                    let scale=Scale2dRenderToCanvas1d{
+                    let scaleC=Scale2dRenderToCanvas1d{
                         x: 0u32,
                         y: 0u32,
                         w:800u32,
@@ -98,7 +107,40 @@ fn benchmark_scale_2d(ctx: &mut Criterion) {
                         interpolation_filter: Filter::RobidouxFast,
                         scale_in_colorspace: float_space
                     };
-                    ctx.bench_function(&format!("scale_2d w={} && h={} fmt={:?} float_space={:?}",w,h,fmt,float_space), |bn| bn.iter(|| {
+
+                    let scaleRust = ScaleAndRenderParams{
+                        x: 0u32,
+                        y: 0u32,
+                        w:800u32,
+                        h:800u32,
+                        sharpen_percent_goal: 0.0,
+                        interpolation_filter: Filter::RobidouxFast,
+                        scale_in_colorspace: match float_space{
+                            Floatspace::Srgb => WorkingFloatspace::StandardRGB,
+                            Floatspace::Linear => WorkingFloatspace::LinearRGB
+                        }
+                    };
+
+                    let mut group = c.benchmark_group(&format!("scale_2d w={} && h={} fmt={:?} float_space={:?}",w,h,fmt,float_space));
+
+                    // Now we can perform benchmarks with this group
+                    group.bench_function("C", |b| b.iter(|| {
+                        unsafe { assert_eq!(flow_node_execute_scale2d_render1d(c.flow_c(),
+                                                                               &mut a as *mut BitmapBgra,
+                                                                               &mut b as *mut BitmapBgra,
+                                                                               &scaleC),true) }
+                    } ));
+                    group.bench_function("Rust", |b| b.iter(|| {
+                        unsafe { assert_eq!(imageflow_core::graphics::scaling::flow_node_execute_scale2d_render1d(
+                            bitmap_a.get_window_u8().unwrap(),bitmap_b.get_window_u8().unwrap(),&scaleRust),true) }
+                    }));
+
+                    // It's recommended to call group.finish() explicitly at the end, but if you don't it will
+                    // be called automatically when the group is dropped.
+                    group.finish();
+
+
+                    ctx.bench_function(&, |bn| bn.iter(|| {
                         unsafe { assert_eq!(flow_node_execute_scale2d_render1d(c.flow_c(), &mut a as *mut BitmapBgra,&mut b as *mut BitmapBgra,&scale),true) }
                     } ));
                 }
