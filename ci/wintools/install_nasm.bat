51fa7a05adfafabf5188ef570954f3684bda4311
Lilith River <lilith@imazen.io>
2025-04-17 21:04:52 -0600

    delete obsolete files

--- ci/wintools/install_nasm.bat
+++
@@ -1,4 +0,0 @@
-curl -L -o nasminst.exe http://www.nasm.us/pub/nasm/releasebuilds/2.12.02/win64/nasm-2.12.02-installer-x64.exe
-start /wait nasminst.exe /S
-set PATH=%PATH%;C:\Program Files (x86)\NASM;
-
--- ci/wintools/win_verify_tools.bat
+++
@@ -1,7 +0,0 @@
-
-echo ============ VERIFY TOOLS REPORT ==================
-echo RUST MUST BE 1.28-beta or later
-rustc -V
-cargo -V
-
-echo ============ END VERIFY TOOLS REPORT ==============
