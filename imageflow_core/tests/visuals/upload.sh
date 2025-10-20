a710231e805ffa0ba9abc5bdba3efd08d781e80d
Lilith River <lilith@imazen.io>
2025-10-20 11:40:21 -0600

    Update upload.sh

--- imageflow_core/tests/visuals/upload.sh
+++ imageflow_core/tests/visuals/upload.sh
@@ -15,14 +15,21 @@ echo "You probably want to run aws configure first"
 
 if command -v aws --version &> /dev/null; then
 
+    ANY_FAILED=0
     cd $(dirname "$0")
     while read -r line; do
         echo "Uploading $line"
         aws s3 cp ./"$line" s3://imageflow-resources/visual_test_checksums/
+        if [ $? -ne 0 ]; then
+            ANY_FAILED=1
+        fi
     done < to_upload.txt
 
-    rm missing_on_s3.txt
+    if [ $ANY_FAILED -eq 0 ]; then
+        rm missing_on_s3.txt
+        rm to_upload.txt
+    fi
 else
-    echo "aws not installed, try: sudo snap install aws-cli --classic"
+    echo "aws not installed, try: sudo snap install aws-cli --classic  then aws configure"
     exit 1
 fi
